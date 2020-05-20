e6382fc9550074fb1dcf91fa168114c40734baf0
Matthew Pecarina <mattp@Matthews-MacBook-Pro.local>
2020-05-20 00:06:33 -0500

    fix default static dir

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ export const initApps = (middleware: Koa.Middleware[]): Koa[] => {
   }
   app.use(
     require("koa-static-server")({
-      rootDir: process.env.STATIC_DIR || "../../../dist",
+      rootDir: process.env.STATIC_DIR || "../../../../dist",
       rootPath: process.env.ROOT_PATH || "/",
     }),
   )
